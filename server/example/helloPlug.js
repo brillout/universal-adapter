2d3647d71a07901b8804e9476b80dd5c4007999c
Romuald Brillout <github.com@brillout.com>
2018-12-28 00:08:26 +0100

    [code-removal] remove extended example

--- server/example/helloPlug.js
+++ server/example/helloPlug.js
@@ -1,34 +1,13 @@
-const parseUri = require('@brillout/parse-uri');
-const computeHash = require('./computeHash');
-
 module.exports = helloPlug;
 
-async function helloPlug(requestContext) {
-  const {url, method} = requestContext;
-  const {pathname} = parseUri(url);
-
-  if( method!=='GET' ) {
+async function helloPlug({url, method}) {
+  if( method!=='GET' || !url.startsWith('/hello/') ) {
     return null;
   }
-  if( pathname==='/' ) {
-    return {
-      body: [
-        "<html>",
-        "<a href='/hello/alice'>/hello/alice</a>",
-        "<br/>",
-        "<a href='/hello/jon'>/hello/jon</a>",
-        "</html>",
-      ].join('\n')
-    }
-  }
-  if( !pathname.startsWith('/hello/') ) {
-    return null;
-  }
-  const body = 'hello '+pathname.slice('/hello/'.length);
   return {
-    body,
+    body: 'hello '+url.slice('/hello/'.length),
     headers: [
-      {name: 'ETag', value: '"'+computeHash(body)+'"'},
-    ],
+      {name: 'Cache-Control', value: 'public, max-age=31536000, immutable'}
+    ]
   };
 }
